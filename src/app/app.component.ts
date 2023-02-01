2f7ff2c69a5d476a33dc70d10199d8a906d16f09
Lenna <pampam32121@gmail.com>
2023-02-01 13:02:01 +0200

    created template project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: 'hinv-root',
+  // templateUrl: './app.component.html',
+  template: `<h1>Hello from template!</h1>
+  <p>Angular is Awesome</p>`,
+  // styleUrls: ['./app.component.scss'],
+  styles: [`h1 {color: red;}`],
 })
 export class AppComponent {
   title = 'hotelinventory';
